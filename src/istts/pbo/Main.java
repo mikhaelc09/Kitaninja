d506dd11ef19411c93b7c9917de771c4d5651f14
mikhaelc09 <82882832+mikhaelc09@users.noreply.github.com>
2021-05-04 19:40:23 +0700

    Frame Size Check

--- src/istts/pbo/Main.java
+++ src/istts/pbo/Main.java
@@ -3,6 +3,6 @@ package istts.pbo;
 public class Main {
 
     public static void main(String[] args) {
-
+        StartPage s = new StartPage();
     }
 }
---
+++ src/istts/pbo/StartPage.java
@@ -0,0 +1,15 @@
+package istts.pbo;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class StartPage extends JFrame {
+    public StartPage() {
+        setSize(new Dimension(1280,720));
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(Color.ORANGE);
+        setVisible(true);
+    }
+}
