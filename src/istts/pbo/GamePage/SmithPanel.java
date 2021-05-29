2bc78014ffaa0318beffa8571b4778f0d76e2693
Soeg-Cronus <83352641+Soeg-Cronus@users.noreply.github.com>
2021-05-29 13:12:50 +0700

    smith kruang lbih selesai(?) tinggal masukno pic

--- src/istts/pbo/GamePage/SmithPanel.java
+++ src/istts/pbo/GamePage/SmithPanel.java
@@ -1,6 +1,8 @@
 package istts.pbo.GamePage;
 
 import istts.pbo.Players.Player;
+import istts.pbo.Players.skilltrees.skills.Skill;
+
 import javax.swing.*;
 import javax.imageio.ImageIO;
 
@@ -13,13 +15,13 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class SmithPanel extends JPanel{
-    ArrayList<skillLabel> lbSkill =new ArrayList<>();
+    ArrayList<upgradelabel> lbItemUpgrade =new ArrayList<>();
     JLabel TombolBack;
     JLabel lbItem;
     JLabel listitem;
     JLabel Equipment;
     int uang=0;
-    JLabel lbDeskrpsi;
+    JLabel lbIconItemSmith;
     JLabel btUp;
     JLabel Gold;
     JLabel Uangmu;
@@ -63,10 +65,12 @@ public class SmithPanel extends JPanel{
         Equipment.setFont(fontlb);
         Equipment.setForeground(Color.WHITE);
         Equipment.setBounds((450-fm.stringWidth(Equipment.getText()))/2, 245, fm.stringWidth(Equipment.getText()), fm.getHeight());
-        lbDeskrpsi = new JLabel();
-        lbDeskrpsi.setBackground(Color.blue);
-        lbDeskrpsi.setOpaque(true);
-        lbDeskrpsi.setBounds(125, 150, 200, 200);
+     //icon item
+        lbIconItemSmith = new JLabel();
+        lbIconItemSmith.setBackground(Color.blue);
+        lbIconItemSmith.setOpaque(true);
+        lbIconItemSmith.setBounds(125, 150, 200, 200);
+
         btUp = new JLabel("Upgrade");
         btUp.setBounds(125,600,200,50);
         btUp.setBackground(Color.orange);
@@ -95,7 +99,7 @@ public class SmithPanel extends JPanel{
         a1.add(btUp);
         a1.add(Uangmu);
         a1.add(Gold);
-        a1.add(lbDeskrpsi);
+        a1.add(lbIconItemSmith);
     }
 
     private void initc2(Player p){
@@ -103,12 +107,61 @@ public class SmithPanel extends JPanel{
         lbItem.setText("Equipment List");
         lbItem.setHorizontalAlignment(SwingConstants.CENTER);
         lbItem.setVerticalAlignment(SwingConstants.NORTH);
-        lbItem.setBackground(Color.black);
+        lbItem.setBackground(Color.white);
         lbItem.setFont(fontlb);
         lbItem.setForeground(Color.WHITE);
         lbItem.setOpaque(true);
         lbItem.setBounds(30,35, 750,650);
+        for (int i = 0; i < 25; i++) {
+            lbItemUpgrade.add(new upgradelabel());
+            lbItemUpgrade.get(i).setText(i+"");
+            lbItemUpgrade.get(i).setCursor(new Cursor(Cursor.HAND_CURSOR));
+            upgradelabel x = lbItemUpgrade.get(i);
+            int sel = i;
+            lbItemUpgrade.get(i).addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    lbIconItemSmith.setIcon(resizeIcon((ImageIcon) x.getIcon(),120,120));
+                    lbIconItemSmith.setBounds((450-fm.stringWidth(lbIconItemSmith.getText()))/2,
+                            245,
+                            fm.stringWidth(lbIconItemSmith.getText()),
+                            fm.getHeight());
+                }
+            });
+        }
+        lbItemUpgrade.get(0).setBounds(30 + 45,70+65,100,100);
+        lbItemUpgrade.get(1).setBounds(30 + 195, 70+65, 100, 100);
+        lbItemUpgrade.get(2).setBounds(30 + 345, 70+65, 100, 100);
+        lbItemUpgrade.get(3).setBounds(30 + 495, 70+65, 100,100);
+        lbItemUpgrade.get(4).setBounds(30 + 645,70+65, 100,100);
 
+        lbItemUpgrade.get(5).setBounds(30 + 45,180+65, 100,100);
+        lbItemUpgrade.get(6).setBounds(30 + 195, 180+65, 100,100);
+        lbItemUpgrade.get(7).setBounds(30 + 345,180+65, 100,100);
+        lbItemUpgrade.get(8).setBounds(30 + 495,180+65, 100,100);
+        lbItemUpgrade.get(9).setBounds(30 + 645, 180+65, 100,100);
+
+        lbItemUpgrade.get(10).setBounds(30 + 45,290+65, 100,100);
+        lbItemUpgrade.get(11).setBounds(30 + 195,290+65, 100,100);
+        lbItemUpgrade.get(12).setBounds(30 + 345, 290+65, 100,100);
+        lbItemUpgrade.get(13).setBounds(30 + 495,290+65, 100,100);
+        lbItemUpgrade.get(14).setBounds(30 + 645,290+65, 100,100);
+
+        lbItemUpgrade.get(15).setBounds(30 + 45,400+65, 100,100);
+        lbItemUpgrade.get(16).setBounds(30 + 195,400+65, 100,100);
+        lbItemUpgrade.get(17).setBounds(30 + 345,400+65, 100,100);
+        lbItemUpgrade.get(18).setBounds(30 + 495,400+65, 100,100);
+        lbItemUpgrade.get(19).setBounds(30 + 645,400+65, 100,100);
+
+        lbItemUpgrade.get(20).setBounds(30 + 45,510+65, 100,100);
+        lbItemUpgrade.get(21).setBounds(30 + 195,510+65, 100,100);
+        lbItemUpgrade.get(22).setBounds(30 + 345,510+65, 100,100);
+        lbItemUpgrade.get(23).setBounds(30 + 495,510+65, 100,100);
+        lbItemUpgrade.get(24).setBounds(30 + 645,510+65, 100,100);
+
+        for (int i = 0; i < 25; i++) {
+            a2.add(lbItemUpgrade.get(i));
+        }
         a2.add(lbItem);
     }
     protected void paintComponent(Graphics g) {
@@ -124,6 +177,31 @@ public class SmithPanel extends JPanel{
             e.printStackTrace();
         }
     }
+    public ImageIcon resizeIcon(String im , int w, int h){
+        return new ImageIcon(new ImageIcon(im).getImage().getScaledInstance(w,h,Image.SCALE_DEFAULT));
+    }
 
+    public ImageIcon resizeIcon(ImageIcon im , int w, int h){
+        return new ImageIcon(im.getImage().getScaledInstance(w,h,Image.SCALE_DEFAULT));
+    }
 }
 
+class upgradelabel extends JLabel{
+    int cost;
+
+    public upgradelabel() {
+        cost=0;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public upgradelabel(int cost) {
+        this.cost = cost;
+    }
+}
