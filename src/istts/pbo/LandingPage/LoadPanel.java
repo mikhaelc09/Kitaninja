f4d4fe510811d12dd72548f8ce45716bfa2313de
mikhaelc09 <82882832+mikhaelc09@users.noreply.github.com>
2021-06-20 15:02:34 +0700

    init loadPanel

---
+++ src/istts/pbo/LandingPage/LoadPanel.java
@@ -0,0 +1,14 @@
+package istts.pbo.LandingPage;
+
+import javax.swing.*;
+
+public class LoadPanel extends JPanel {
+
+    public LoadPanel() {
+        init();
+    }
+
+    private void init(){
+
+    }
+}
