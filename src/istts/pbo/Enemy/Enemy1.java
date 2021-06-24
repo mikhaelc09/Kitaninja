c31342cfeeebc02aec1258e192e28836a1c1bea3
mikhaelc09 <82882832+mikhaelc09@users.noreply.github.com>
2021-06-24 14:44:44 +0700

    enemy name

--- src/istts/pbo/Enemy/Enemy1.java
+++ src/istts/pbo/Enemy/Enemy1.java
@@ -1,6 +1,7 @@
 package istts.pbo.Enemy;
 
 public class Enemy1 extends StatEnemy{
+    String name = "Bomberman";
 
     public Enemy1() {
         super(400,300,30,10,5,10,10,"src/istts/pbo/res/sprites/enemy1.gif");
--- src/istts/pbo/Enemy/Enemy2.java
+++ src/istts/pbo/Enemy/Enemy2.java
@@ -1,6 +1,8 @@
 package istts.pbo.Enemy;
 
 public class Enemy2 extends StatEnemy{
+    String name ="Bad Plushie Lion";
+
     public Enemy2() {
         super(500,200,20,15,10,10,0,"src/istts/pbo/res/sprites/enemy2.gif");
     }
